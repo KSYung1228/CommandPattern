2796b10692476c45d540905adb00426ea0237185
KSY <109911110+KSYung1228@users.noreply.github.com>
2023-11-13 15:42:50 +0800

    V1

---
+++ ExitCommand.java
@@ -0,0 +1,16 @@
+public class ExitCommand implements Command {
+    @Override
+    public void exit() {
+        System.exit(0);
+    }
+
+    @Override
+    public void undo() {
+        // Nothing to do
+    }
+
+    @Override
+    public void execute() {
+        // Nothing to do
+    }
+}
